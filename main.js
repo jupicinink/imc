289cd43a79427267027e525242b81c71a45b8970
Julia da SIlva Picinini <123598398+jupicinink@users.noreply.github.com>
2024-08-21 16:05:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form")
 
-form.addEventListener('submit', function (event){
-    event.preventDefaut();
+form.addEventListener('submit', function(e){
+    e.preventDefault();
     const inputPeso = e.target.querySelector('#peso');
-    const inputAltura = e.target.querySelector('#altura');
-
+    const inputAlt = e.target.querySelector('#altura');
+    // pegando o valor e tranformando em number
     const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
-
-    if(!peso){
-    setResultado('peso invalido', false)
-    return;
-    }
-
-    if(!altura){
-        setResultado('altura invalido', false)
+    const altura = Number(inputAlt.value);
+    // caso seja NaN
+    if (!peso){
+        setResultado('Peso Invalido', false)
         return;
     }
-    console.log('chegeui');
-})
+    if (!altura){
+        setResultado('Altura Invalido', false)
+        return;
+    }
+    
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
 
-function criarParagrafo(){
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+    setResultado(msg, true);
+    
+});
+
+function getNivelImc(imc){
+    const nivel = ['Abeixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau I', 'Obesidade grau II', 'Obesidade grau III']
+    if(imc >= 39.9) return nivel[5];
+    if(imc >= 34.9) return nivel[4];
+    if(imc >= 29.9) return nivel[3];
+    if(imc >= 24.9) return nivel[2];
+    if(imc >= 18.5) return nivel[1];
+    if(imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura){
+    const imc = peso / altura**2;
+    return imc.toFixed(2);
+}
+
+function criaP(){
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado(msg , isValid){
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML  = '';
-
-    const p = criarParagrafo();
-    p.innerHTML = msg
+function setResultado (msg, isValid)  {
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = '';
+    const p = criaP();
+    if(isValid) {
+        p.classList.add('paragrafo-resultado');
+    }
+    else {
+        p.classList.add('bad')
+    }
     resultado.appendChild(p)
+
 }
